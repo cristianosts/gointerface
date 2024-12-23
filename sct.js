1e769429b4d60a348774fd47bde2d1743fd4f15a
cristianosts <cristianosantosreal@gmail.com>
2024-12-23 04:33:07 -0300

    act

--- sct.js
+++ sct.js
@@ -4,32 +4,36 @@ let container = document.querySelector('.container')
 let items = container.querySelectorAll('.list .item')
 let indicator = document.querySelector('.indicators')
 let dots = indicator.querySelectorAll('ul li')
+let list = container.querySelector('.list')
 
 let active = 0 
 let firstP = 0
 let lastP = items.length - 1
 
-nextButton.onclick = () => {
+function setSlider() {
     let itemOld = container.querySelector('.list .item.active')
 
     itemOld.classList.remove('active')
-
-    active = active + 1 > lastP ? 0 : active + 1
-
-    items[active].classList.add('active')
 
     let dotsOld = indicator.querySelector('ul li.active')
 
     dotsOld.classList.remove('active')  
+    dots[active].classList.add('active')
+
+    indicator.querySelector('.numbers').innerHTML = '0' + (active + 1)
+
+}
+
+nextButton.onclick = () => {
+    active = active + 1 > lastP ? 0 : active + 1
+    setSlider()
+    items[active].classList.add('active')
+
 }
 
 prevButton.onclick = () => {
-    let itemOld = container.querySelector('.list .item.active')
-
-    itemOld.classList.remove('active')
-
     active = active - 1 < firstP ? lastP : active - 1
-
+    setSlider()
     items[active].classList.add('active')
 
 }
